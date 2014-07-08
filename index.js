7cf797ad2f18eb2b198df6b4cbdaa14d20d7ffb8
Christoph Hermann <schtoeffel@gmail.com>
2014-07-08 09:30:01 +0200

    update

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ Generator.prototype.createData = function(jsonFilePath) {
   var q = Q.defer(),
     data = require(process.cwd() + '/' + this.config.json);
   if (this.filters) {
+    if (!_.isArray(this.filters)) {
+        this.filters = [this.filters];
+    }
     data = _.filter(data, function(item) {
       return _.any(this.filters, function(filter) {
         var key = _.keys(filter)[0];
+        if (!_.isArray(filter[key])) {
+            filter[key] = [filter[key]];
+        }
         return _.contains(filter[key], item[key]);
       }, this);
     }, this);
